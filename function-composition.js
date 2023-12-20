5f5441046dd723bb95314d17051a1c049d9ba779
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-20 12:41:16 +0300

    Compose using lodash functions.

--- function-composition.js
+++ function-composition.js
@@ -1,11 +1,19 @@
+import _ from 'lodash/fp';
+
 const trim = (str) => str.trim();
 const toLowerCase = (str) => str.toLowerCase();
 const quote = (str) => `<q>${str}</q>`;
 const wrapInDiv = (str) => `<div>${str}</div>`;
 
-const result = wrapInDiv(
+let result = wrapInDiv(
   quote(toLowerCase(trim('    WEL COME TO REDUX.      ')))
 );
+
+const composer = _.compose(wrapInDiv, quote, toLowerCase, trim);
+result += composer('   USING LODASH COMPOSE FUNCTION: WEL COME TO REDUX.  ');
+
+const pipe = _.pipe(trim, toLowerCase, quote, wrapInDiv);
+result += pipe('   USING LODASH PIPE FUNCTION: WEL COME TO REDUX.  ');
 
 export default function showMessage() {
   return result;
