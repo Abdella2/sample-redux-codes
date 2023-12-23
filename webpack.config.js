fb6ba631f3a694c3d3d9abf607e5483f4ad2aee0
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-23 15:01:44 +0300

    Enabling redux devtool tracing.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-        port: 9000
-      },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-      },
-  };
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+    port: 9000
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devtool: 'source-map'
+};
