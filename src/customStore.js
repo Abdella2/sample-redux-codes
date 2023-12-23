1c24e355a2f8f4e0c39536ae3ece3f403a4c84f0
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-23 11:43:55 +0300

    Subscribe to customer store.

--- src/customStore.js
+++ src/customStore.js
@@ -1,6 +1,7 @@
 import reducer from './reducer';
 function createStore(reducer) {
   let state;
+  const subscriptions = [];
 
   function getState() {
     return state;
@@ -8,11 +9,19 @@ function createStore(reducer) {
 
   function dispatch(action) {
     state = reducer(state, action);
+    subscriptions.forEach((callback) => callback());
+  }
+
+  function subscribe(subscription) {
+    subscriptions.push(subscription);
+    const index = subscriptions.length - 1;
+    return () => subscriptions.splice(index, 1);
   }
 
   return {
     getState,
-    dispatch
+    dispatch,
+    subscribe
   };
 }
 
--- src/customerDispatch.js
+++ src/customerDispatch.js
@@ -1,6 +1,11 @@
 import * as actions from './actions';
 import store from './customStore';
 
+const unsubscribe = store.subscribe(() =>
+  console.log('Store changed!', store.getState())
+);
+store.subscribe(() => console.log('Updating the UI.'));
+
 store.dispatch(
   actions.addArticle({
     title: 'customer store article',
@@ -8,8 +13,14 @@ store.dispatch(
   })
 );
 
-console.log(store.getState());
+unsubscribe();
+
+console.log('');
+console.log('After unsubscribing the first subscription.');
+console.log('');
+
+// console.log(store.getState());
 
 store.dispatch(actions.removeArticle(1));
 
-console.log(store.getState());
+// console.log(store.getState());
