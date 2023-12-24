eddc8dbf917ce69e39e1cc25dd851dc61dce54fd
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-24 10:47:09 +0300

    Use redux-toolkit configureStore function to create a store.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,6 @@
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './articles';
 
-export default function configureStore() {
-  const store = createStore(
-    reducer,
-    devToolsEnhancer({
-      trace: true
-    })
-  );
-
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
