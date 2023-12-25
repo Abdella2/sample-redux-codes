101938577a87df632fd7006304ea2764248380fa
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-25 16:19:23 +0300

    Use redux-toolkit thunk middleware.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import func from './middleware/func';
 import logger from './middleware/logger';
 import reducer from './reducer';
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: () => [logger({ destination: 'console' }), func]
+    middleware: (getDefaultMiddleware) => [
+      ...getDefaultMiddleware(),
+      logger({ destination: 'console' })
+    ]
   });
 }
